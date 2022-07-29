c5eeae70563fed4218d7dfe75afd0adda4a93607
Igor Kupchanu <igorzm2142@yandex.ru>
2022-07-29 19:04:38 +0300

    Adding files

---
+++ fibonacci/fibonacci/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> fib(int n);
+
+int main()
+{
+	  std::cout << "Enter Fibonacci number: ";
+	  int numFib;
+	  std::cin >> numFib;
+
+	  std::vector<int> resultFib = fib(numFib);
+	  for (const auto& i : resultFib) {
+			std::cout << "Fibonacci = " << i << '\n';
+	  }
+	  std::cout << '\n';
+	  std::cout << "Fibonacci(" << numFib << ") = " << resultFib.back() << '\n';
+
+	  system("pause");
+	  return 0;
+}
+
+std::vector<int> fib(int num)
+{
+	  std::vector<int> arr;
+	  const int minNumFib = 0;
+	  const int maxNumFib = 30;
+	  if (num < minNumFib || num > maxNumFib) {
+			std::cerr << "Error !!! number Fibonacci must not be < 0 or > 30\n";
+			arr.push_back(num);
+			return arr;
+	  }
+	  else if (num == 0) {
+			arr.push_back(0);
+			return arr;
+	  }
+	  else {
+			size_t numStep = 1;
+			size_t numStep2 = 2;
+			arr.push_back(0);
+			arr.push_back(1);
+			for (size_t i = 2; i <= num; ++i) {
+				  arr.push_back(arr[i - numStep] + arr[i - numStep2]);
+			}
+	  }
+
+	  return arr;
+}
